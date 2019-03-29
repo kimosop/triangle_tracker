b2a57c21e973b0fe70810e5843bb1d19173adc95
Victor Kimosop Chepkoit <VCHEPKOIT@safaricom.co.ke>
2019-03-29 16:35:51 +0300

    completed working version 1

--- js/triangle_tracker.js
+++ js/triangle_tracker.js
@@ -5,3 +5,33 @@ var triangle = function(sideOne, sideTwo, sideThree){
     return "These sides do not a triangle make.";
   }
 };
+var triangleType = function(sideOne, sideTwo, sideThree){
+  if (sideOne===sideTwo && sideTwo===sideThree) {
+    return "equilateral triangle";
+  } else if (sideOne===sideTwo || sideOne===sideThree || sideThree===sideTwo) {
+    return "isosceles triangle";
+  } else {
+    return "scalene triangle";
+  }
+};
+
+var isTriangle = function(sideOne, sideTwo, sideThree) {
+  return sideOne <= (sideTwo + sideThree) && sideTwo <= (sideThree + sideOne) && sideThree <= (sideOne + sideTwo)
+};
+
+$(document).ready(function() {
+  $("form#triangle").submit(function(event) {
+
+    var sideOne = parseInt($("input#sideOne").val());
+    var sideTwo = parseInt($("input#sideTwo").val());
+    var sideThree = parseInt($("input#sideThree").val());
+    var result = triangle(sideOne, sideTwo, sideThree);
+
+    sideOne = parseInt($("input#sideOne").val(""));
+    sideTwo = parseInt($("input#sideTwo").val(""));
+    sideThree = parseInt($("input#sideThree").val(""));
+
+    $("#result").append(result);
+    event.preventDefault();
+  });
+});
